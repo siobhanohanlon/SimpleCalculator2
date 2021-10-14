e4001685f3c29a61066c7d7f712facc59fddf70d
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-14 15:18:49 +0100

    Add project files.

---
+++ SimpleCalculator2/Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Main
+void main()
+{
+	// Declare variables
+	int num, sum, average, validint, smallest, largest;
+	int i = 0;
+
+	//Create an array of numbers enetered
+	int[] numbs = new int[];
+
+	//Until -1 is entered
+	while (num != -1)
+	{
+		printf("Please enter number %d:\t", i);
+		scanf("%d", &num);
+
+		numbs[i] = num;
+		i++;
+	}
+
+	
+}
