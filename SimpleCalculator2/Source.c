6b0fc026e54e13ab608e57bb130796ad17f9c7e0
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-14 19:05:55 +0100

    Finished Code and Calculations needed

--- SimpleCalculator2/Source.c
+++ SimpleCalculator2/Source.c
@@ -4,7 +4,7 @@
 void main()
 {
 	// Declare variables
-	int input, noIterations, sum = 0, average, validint, smallest, largest;
+	int input, noIterations, sum = 0, average, valid=0 , smallest=0, largest=0;
 	int i = 1;
 
 	//Tell User how to work code
@@ -13,18 +13,13 @@ void main()
 	printf("Please enter number %d:\t", i);
 	scanf("%d", &input);
 	sum += input;
-	smallest = input;
-	largest = input;
+	smallest += input;
+	largest += input;
 	i++;
-
-	//This way the minus number needed to finish wont take from number
-	if (input < 0)
-	{
-		printf("Finished Calculations");
-	}
+	valid++;
 
 	//Until -1 is entered
-	while (input >= 0);
+	while(input >= 0)
 	{
 		printf("Please enter number %d:\t", i);
 		scanf("%d", &input);
@@ -33,6 +28,7 @@ void main()
 		if (input >= 0)
 		{
 			sum += input;
+			++valid;
 			i++;
 
 			//If input is now largest number
@@ -49,14 +45,17 @@ void main()
 		}
 	} 
 
+	printf("\nFinished Calculations\n");
+
 	//Calculations
 	if (sum > 0)
 	{
 		average = sum / (i - 1);
 		//Display Calculations
 		printf("\nSum of all numbers is:\t\t%d", sum);
-		printf("\nAverage of all numbers is:\t%d\n\n", average);
-		printf("\nSmallest number entered is:\t%d\n\n", smallest);
-		printf("\nLargest number entered is:\t%d\n\n", largest);
+		printf("\nAverage of all numbers is:\t%d", average);
+		printf("\nSmallest number entered is:\t%d", smallest);
+		printf("\nLargest number entered is:\t%d", largest);
+		printf("\nNumber of valid ints entered:\t%d\n\n", valid);
 	}
 }
