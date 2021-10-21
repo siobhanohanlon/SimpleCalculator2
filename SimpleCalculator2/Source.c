f4a0ed24352cb5863ab7ff761c1446a782258ba7
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-21 14:59:59 +0100

    Changes made for tidier code

--- SimpleCalculator2/Source.c
+++ SimpleCalculator2/Source.c
@@ -4,7 +4,7 @@
 void main()
 {
 	// Declare variables
-	int input, noIterations, sum = 0, average, valid=0 , smallest=0, largest=0;
+	int input, sum = 0, average, valid=0 , smallest, largest;
 	int i = 1;
 
 	//Tell User how to work code
@@ -14,54 +14,52 @@ void main()
 	printf("Please enter number %d:\t", i);
 	scanf("%d", &input);
 
-	//Add input to sum
-	sum += input;
-
-	//Change smallest and largest to entered number as if done after this, they're the values
-	smallest += input;
-	largest += input;
-
-	//Update counter and number of valid numbers entered
-	i++;
-	valid++;
+	if (i == 1)
+	{
+		smallest = input;
+		largest = input;
+	}
 
 	//Until -1 is entered
-	while(input >= 0)
+	while(input > 0)
 	{
+		//Update variables
+		i++;
+		sum += input;
+
+		//If input is now largest number
+		if (input > largest)
+		{
+			largest = input;
+		}
+
+		//If input is now smallest number
+		if (input < smallest)
+		{
+			smallest = input;
+		}
+
 		//Ask user to enter rest of numbers
 		printf("Please enter number %d:\t", i);
 		scanf("%d", &input);
-
-		//This way the minus number needed to finish wont take from number
-		if (input >= 0)
-		{
-			//Update Variables as input was positive
-			sum += input;
-			++valid;
-			i++;
-
-			//If input is now largest number
-			if (input > largest)
-			{
-				largest = input;
-			}
-
-			//If input is now smallest number
-			if (input < smallest)
-			{
-				smallest = input;
-			}
-		}
 	} 
 
 	//Display text to show no more numbers to be entered
 	printf("\nFinished Calculations\n");
 
+	//If stoped on first number
+	if (i == 2 && input <= 0)
+	{
+		printf("\nERROR!!!! No Numbers to Compute\n");
+	}
+
 	//Calculations, only done and displayed if sum over 1, meaning a positive number was entered
 	if (sum > 0)
 	{
-		average = sum / (i - 1);
-
+		//Update Variables
+		valid = --i;
+		average = sum/valid;
+		
 		//Display Calculations
 		printf("\nSum of all numbers is:\t\t%d", sum);
 		printf("\nAverage of all numbers is:\t%d", average);
